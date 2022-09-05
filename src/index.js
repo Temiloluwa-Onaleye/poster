b37ea486ca2a2833c5aa017ccef6ba9d656242a6
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2022-09-05 14:16:37 +0100

    React-exercise

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// function Poster({ post }) {
+//   post = {
+//     image:
+//       "https://cdn.britannica.com/60/5760-004-FDD0E2AB/Apples.jpg?s=1500x700&q=85",
+//     title: "APPLE",
+//     text: "This is a fruit",
+//   };
+//   return (
+//     <div>
+//       <img src={post.image} alt="fruit" />
+//       <h1>{post.title}</h1>
+//       <p>{post.text}</p>
+//     </div>
+//   );
+// }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// function IconButton({ children }) {
+//   return (
+//     <button>
+//       <i class="target-icon" />
+//       {children}
+//     </button>
+//   );
+// }
+
+function NavItem({ url, text }) {
+  return <a href={url}>{text}</a>;
+}
+
+function Nav({ children }) {
+  React.Children.forEach(children);
+  console.log(children);
+  return (
+    <div>
+      <NavItem url="/" text="Home" />
+      <NavItem url="/about" text="About" />
+      <a href="/contact">Contact</a>
+    </div>
+  );
+}
+
+ReactDOM.render(<Nav children={NavItem} />, document.querySelector("#root"));
